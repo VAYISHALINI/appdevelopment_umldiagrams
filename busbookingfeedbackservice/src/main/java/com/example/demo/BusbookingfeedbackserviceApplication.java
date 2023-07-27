42bd72f232d901a65ebab20c8342c2750a6eb571
VAYISHALINI <113620656+VAYISHALINI@users.noreply.github.com>
2023-07-27 09:33:42 +0530

    Add files via upload

---
+++ busbookingfeedbackservice/src/main/java/com/example/demo/BusbookingfeedbackserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusbookingfeedbackserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusbookingfeedbackserviceApplication.class, args);
+	}
+
+}
